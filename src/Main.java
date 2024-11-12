82093f6f3cb11edd318de4ad8cab9170e11164d4
Alexander Ryzhakov <aryzhakov87@gmail.com>
2024-11-12 13:33:17 +1000

    Rework1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String[] args){
-        double balance = 100.30; //Баланс счета в рублях
-        int payment = 1500; //Платёж в рублях
+    public static void main(String[] args) {
+        double balance = 100; //Баланс счета в рублях
+        int payment = 1100; //Платёж в рублях
         int bonus = payment / 100;
-        if (bonus > 0){
+        if (bonus >= 10) {
             System.out.println("Ваш бонус составил:" + bonus + "рублей(ля)");
             System.out.println("Ваш баланс:" + (balance + payment + bonus) + "рублей(ля)");
         } else {
